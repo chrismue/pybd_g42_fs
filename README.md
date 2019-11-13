42bb7929cad8c1095ae6a4f8a49454e19c62e454
christian.mueller <christian.mueller@noser.com>
2019-11-13 21:20:58 +0100

    Removing some unused code references

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# pybd_g42_fs
-Pyboard D files for Guild42
+# Guild42 MicroPython Demo
 
+Demo application for Pyboard D files, used at presentation at Guild42
+
+The application reads a accelerometer from I2C and represents the rotation on a LED36-Tile. 
+In addition, a static single-page-application from the SD-card is hosted over the WLan-Access-Point 'Guild42Mp'. 
+
+Use flash.py to copy all necessary files, connect to the websocket and open http://192.168.4.1/index.html
+
+_Note that this should code should represent a __Prototype__ Application!_
+
+Blog Post about MicroPython: 
 https://blog.noser.com/embedded-prototyping-mit-micropython/
--- root/main.py
+++ root/main.py
@@ -1,69 +1,51 @@
 import pyb
-import time
-
 import network
 
 import led36
 from lsm9ds1 import LSM9DS1
 
-import micropython
 import uasyncio
 from uasyncio.websocket.server import WSReader, WSWriter
 from uasyncio.queues import Queue
 
+import picoweb
+
 import os
 
-import ure as re
+measurement_queue = Queue()
 
-q = Queue()
 
 if pyb.SDCard().present():
     os.mount(pyb.SDCard(), '/sd')
-    print("Mounted SD Card:")
-    print(os.listdir("/sd/"))
-    print(os.listdir("/sd/web"))
+    print("Mounted SD Card")
 else:
     print("No SD Card present!")
 
-class LedTile:
-    def __init__(self):
-        print("INIT LEDTILE")
-        led36.brightness(100)
-        led36.illu(0, 0, 0)
 
-    def set_color(self, r, g, b):
-        led36.illu(r, g, b)
+def init_led_tile():
+    led36.brightness(100)
+    led36.illu(0, 0, 0)
 
 
-class ThreadedMeasuring:
-    def __init__(self, led_tile):
-        # Initialize internal variables
-        self._r = 0
-        self._g = 0
-        self._led_tile = led_tile
-
-        # Initialize Inertial Module
-        i2c_y = pyb.I2C(2, pyb.I2C.MASTER, baudrate=100000)
-        self._lsm9d1 = LSM9DS1(i2c_y, dev_acc_sel=0x6A, dev_gyr_sel=0x6A, dev_mag_sel=0x1C)
+def init_inertia_module():
+    i2c_y = pyb.I2C(2, pyb.I2C.MASTER, baudrate=100000)
+    lsm9d1 = LSM9DS1(i2c_y, dev_acc_sel=0x6A, dev_gyr_sel=0x6A, dev_mag_sel=0x1C)
+    return lsm9d1
 
 
 @uasyncio.coroutine
-def measure(meas):
+def measure(inertia_module):
     while 1:
-        # print("Measuring...")
-        acc = meas._lsm9d1.accel.xyz()
-        r = min(int(acc[1] * 512), 255) if acc[1] > 0 else 0
+        acc = inertia_module.accel.xyz()
+        r = min(int(acc[1] * 512), 255) if acc[1] > 0 else 0  # scale to 0 .. 255
         g = min(int(-acc[1] * 512), 255) if acc[1] < 0 else 0
-        meas._led_tile.set_color(r, g, 0)
-        x = min(max(acc[0] * 180, -90), 90)
+        led36.illu(r, g, 0)  # set color on LED tile
+        x = min(max(acc[0] * 180, -90), 90)  # scale to -90.0 .. +90.0 degrees
         y = min(max(acc[1] * 180, -90), 90)
         z = min(max(acc[2] * 180, -90), 90)
-        # print("Measured.")
-        if q.empty():
-            yield from q.put((r,g,x,y,z))
-            # print("Put In Queue")
+        if measurement_queue.empty():
+            yield from measurement_queue.put((r,g,x,y,z))
         yield
-
 
 
 class AccessPoint(network.WLAN):
@@ -76,34 +58,6 @@ class AccessPoint(network.WLAN):
         self.active(1)  # enable the AP
         print("Started Access point: %s" % self.SSID)
 
-"""
-def echo(reader, writer):
-    # Consume GET line
-    yield from reader.readline()
-
-    reader = yield from WSReader(reader, writer)
-    writer = WSWriter(reader, writer)
-
-    while 1:
-        l = yield from reader.read(256)
-        print(l)
-        if l == b"\r":
-            await writer.awrite(b"\r\n")
-        else:
-            await writer.awrite(l)
-"""
-
-@uasyncio.coroutine
-def serve(reader, writer):
-    print(reader, writer)
-    print("================")
-    print((yield from reader.read()))
-    with open('www/index.html', 'r') as f:
-        yield from writer.awrite("HTTP/1.0 200 OK\r\n\r\n" + f.read())
-    print("After response write")
-    yield from writer.aclose()
-    print("Finished processing request")
-
 
 @uasyncio.coroutine
 def serve_websocket(reader, writer):
@@ -115,90 +69,32 @@ def serve_websocket(reader, writer):
 
     try:
         while 1:
-            if not q.empty():
+            if not measurement_queue.empty():
                 # print("NotEmpty")
-                r,g,x,y,z = yield from q.get()
+                r,g,x,y,z = yield from measurement_queue.get()
                 await writer.awrite("%d,%d;%f,%f,%f" % (r,g,x,y,z))
             yield
     except Exception as e:
         print("Exception in websocket: " + str(e))
 
-#
-# This is a picoweb example showing a centralized web page route
-# specification (classical Django style).
-#
-# import ure as re
-import picoweb
-
-
-def index(req, resp):
-    # You can construct an HTTP response completely yourself, having
-    # a full control of headers sent...
-    yield from resp.awrite("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n")
-    yield from resp.awrite('<html><meta http-equiv="refresh", content="0;URL=/static/index.html" /></html>')
-
 
 ROUTES = [
-    # You can specify exact URI string matches...
+    # basic sample html at root
     ("/", lambda req, resp: (yield from app.sendfile(resp, "./www/index.html"))),
 ]
 
-print("STARTING")
+
 if __name__ == "__main__":
-    tile = LedTile()
-    meas = ThreadedMeasuring(tile)
+    init_led_tile()
+    inertia_module = init_inertia_module()
     ap = AccessPoint()
 
     app = picoweb.WebApp(__name__, ROUTES)
-    # debug values:
-    # -1 disable all logging
-    # 0 (False) normal logging: requests and errors
-    # 1 (True) debug logging
-    # 2 extra debug logging
-    app.debug = -1
+    app.debug = -1  # disable all logging
 
-    # print("My loop :-)")
     loop = uasyncio.get_event_loop()
-    # loop.call_soon(uasyncio.start_server(serve, "192.168.4.1", 80))
-
     loop.call_soon(uasyncio.start_server(serve_websocket, "192.168.4.1", 500))
-    loop.create_task(measure(meas))
+    loop.create_task(measure(inertia_module))
     app.serve(loop, "192.168.4.1", 80)
-    loop.run_forever()
-    loop.close()
 
-    """
-
-    serve(self, loop, host, port):
-    # Actually serve client connections. Subclasses may override this
-    # to e.g. catch and handle exceptions when dealing with server socket
-    # (which are otherwise unhandled and will terminate a Picoweb app).
-    # Note: name and signature of this method may change.
-    loop.create_task(asyncio.start_server(self._handle, host, port))
-    loop.run_forever()
-
-
-def run(self, host="127.0.0.1", port=8081, debug=False, lazy_init=False, log=None):
-    if log is None and debug >= 0:
-        import ulogging
-        log = ulogging.getLogger("picoweb")
-        if debug > 0:
-            log.setLevel(ulogging.DEBUG)
-    self.log = log
-    gc.collect()
-    self.debug = int(debug)
-    self.init()
-    if not lazy_init:
-        for app in self.mounts:
-            app.init()
-    loop = asyncio.get_event_loop()
-    if debug > 0:
-        print("* Running on http://%s:%s/" % (host, port))
-    self.serve(loop, host, port)
-    loop.close()
-
-
-
-    app.run(host="192.168.4.1", port=80, debug=-1)
-
-"""
+    # never reaches here: picoweb call run_forever of uasyncio loop
